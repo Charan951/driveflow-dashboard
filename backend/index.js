80a5775542eb98077a9ebe73abe89b3e82c26bc6
Charan951 <charanteja@speshway.com>
2026-03-27 11:34:55 +0530

    hi

--- backend/index.js
+++ backend/index.js
@@ -156,6 +156,26 @@ process.on('uncaughtException', (err) => {
   process.exit(1);
 });
 
-server.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
-});
+import cluster from 'cluster';
+import os from 'os';
+
+const numCPUs = os.cpus().length;
+
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+
+  server.listen(PORT, () => {
+    logger.info(`Server running on port ${PORT}`);
+  });
+} else {
+  console.log(`Worker ${process.pid} started`);
+}
